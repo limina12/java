bdfceee00c29ef9988885a55cc58062d61e82d7b
limina12 <34646822+limina12@users.noreply.github.com>
2018-06-26 11:47:37 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+		    int c=0;
+		    for(int j=1;j<=n;j++)
+		    {
+		        if(i%j==0)
+		        {
+		            c++;
+		        }
+		    }
+		    if(c==2)
+		    {
+		        System.out.println(i);
+		    }
+		}
+	}
+}
