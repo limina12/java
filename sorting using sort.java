6f0ec2f5fe77016a3ec3f46b5859b3607811d102
limina12 <34646822+limina12@users.noreply.github.com>
2018-06-28 14:49:27 +0530

    Create sorting using sort.java

---
+++ sorting using sort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class test1 {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+       Arrays.sort(arr);
+        for(i=0;i<arr.length;i++)
+        {
+        System.out.print(arr[i]+" ");      
+    }
+    
+}
+}
